aef670344707d9ab9183a341c38bce20fd2ed54b
Rushikesh2201 <rushikesht@trinesis.com>
2022-06-22 14:25:09 +0530

    Update With Script running at 11am

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cron = require('node-cron');
 
+
 app.use(express.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(express.json());
@@ -11,38 +12,18 @@ app.use(express.json());
 
  const response = app.get('/', async(req,res) => {
     try{
-         const message = cron.schedule('*/5 * * * * *',() =>{
-        res.send("Hello Work..." + new Date())
-        console.log("Hello work" +  new Date())  
-       })         
+         
+         const Schedule = cron.schedule(' * * */11 * * */0-6',() =>{
+            res.send("Hello Work... " + new Date())
+            console.log("Hello work " +  new Date()) 
+       })
+      
+       //message.stop();        
     } 
     catch(err){
         console.log(err);
     }
- });
-
-// const response = app.get('/', (req,res) =>{
-//     try{
-//     res.send("Hello Work...");
-//     }
-//     catch(err){
-//         console.log(err);
-//     }
-// })
-
-//                             // s   m h d m w
-// const message = cron.schedule('*/5 * * * * *',()=>
-// //cron.schedule('00 00 11 * * 0-6', () =>)
-// {
-//     try{
-//         console.log( "Hello Work " +  new Date())
-//     }
-//     catch(err){
-//         console.log(err);
-//         }
-//        }) 
-        //message.stop();
-       //message.start();     
+ });     
 
 
 const port = 5000;
